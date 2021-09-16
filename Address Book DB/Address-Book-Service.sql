9b24214a34ffb400bb98721c4b0a890a3c28b0ea
sheetal-10 <sheetalp257@gmail.com>
2021-09-16 23:43:25 +0530

    Added: SQL Client commands to create a table Address_book

--- Address Book DB/Address-Book-Service.sql
+++ Address Book DB/Address-Book-Service.sql
@@ -1,6 +1,6 @@
 ********************Welcome to Address Book Database problem********************
 
-#UC1-Creates Data base
+######### UC1-Creates Data base ##########
 
 mysql> create database address_book_service;
 Query OK, 1 row affected (0.03 sec)
@@ -29,3 +29,42 @@ mysql> Select database();
 | address_book_service |
 +----------------------+
 1 row in set (0.00 sec)
+
+
+
+########## UC2-creates a Table ##########
+
+mysql> create table address_book(
+    -> first_name varchar(31) not null,
+    -> last_name varchar(31) not null,
+    -> address varchar(150) not null,
+    -> city varchar(31) not null,
+    -> state varchar(31) not null,
+    -> zip int not null,
+    -> phone_number int not null,
+    -> email varchar(50) not null);
+Query OK, 0 rows affected (0.07 sec)
+
+mysql> show tables;
++--------------------------------+
+| Tables_in_address_book_service |
++--------------------------------+
+| address_book                   |
++--------------------------------+
+1 row in set (0.01 sec)
+
+mysql> desc address_book;
++--------------+--------------+------+-----+---------+-------+
+| Field        | Type         | Null | Key | Default | Extra |
++--------------+--------------+------+-----+---------+-------+
+| first_name   | varchar(31)  | NO   |     | NULL    |       |
+| last_name    | varchar(31)  | NO   |     | NULL    |       |
+| address      | varchar(150) | NO   |     | NULL    |       |
+| city         | varchar(31)  | NO   |     | NULL    |       |
+| state        | varchar(31)  | NO   |     | NULL    |       |
+| zip          | int          | NO   |     | NULL    |       |
+| phone_number | int          | NO   |     | NULL    |       |
+| email        | varchar(50)  | NO   |     | NULL    |       |
++--------------+--------------+------+-----+---------+-------+
+8 rows in set (0.01 sec)
+
